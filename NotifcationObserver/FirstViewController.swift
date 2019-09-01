9e910a0e4e313ab571c96750feedabd7f9a98122
Moussa <moussa@MacBook-Pro-van-sos.local>
2019-09-01 15:42:12 +0100

    Initial Commit

---
+++ NotifcationObserver/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NotifcationObserver
+//
+//  Created by Moussa on 01/09/2019.
+//  Copyright © 2019 Moussa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NotifcationObserver/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NotifcationObserver
+//
+//  Created by Moussa on 01/09/2019.
+//  Copyright © 2019 Moussa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
